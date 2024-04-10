0e26a154d1c1b816b9b36dea1c04af3ddcee8292
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2024-04-11 02:29:24 +1000

    groups poc

--- bin/clean-operator.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-oc -n openshift-gitops delete application bootstrap operator operator-instance
-oc -n openshift-operators delete csv openshift-pipelines-operator-rh.v1.14.3
-oc get crds -o name | grep tekton | xargs oc delete
